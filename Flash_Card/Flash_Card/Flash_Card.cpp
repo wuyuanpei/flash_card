a94bc3226adba2f26a20b8f6270f6bd8116c6cde
Yuanpei Wu <yuanpei.wu@wustl.edu>
2020-01-18 00:07:51 -0600

    eight options for mc

--- Flash_Card/Flash_Card/Flash_Card.cpp
+++ Flash_Card/Flash_Card/Flash_Card.cpp
@@ -25,7 +25,7 @@ int main()
 	select_mode();
 }
 void select_mode() {
-	printf("\t       Flash Card (%d words)\n",count_lines);
+	printf("\n\t       Flash Card (%d words)\n",count_lines);
 	printf("\t\t  Developed by RW\n\n");
 	printf("\ta: random words\n");
 	printf("\ts: multiple choices\n");
@@ -117,22 +117,28 @@ void mc() {
 	while (1) {
 		int idx = rand() % count_lines;
 		int len_E = strlen(list[idx].English);
-		printf("\n\n\n");
+		printf("\n\n");
 		for (int i = 0; i < 26 - ((len_E + 1) >> 1); i++) {
 			printf(" ");
 		}
 		printf("%s\n\n", list[idx].English);
-		int a, b, c;
+		int a, b, c, d, e, f, g;
 		do {
 			a = rand() % count_lines;
 			b = rand() % count_lines;
 			c = rand() % count_lines;
-		} while (a == b || a == c || b == c || a == idx || b == idx || c == idx);
-		int position = rand() % 4;
-		int ids[] = { a,b,c };
-		char hints[] = {'a','s','d','f'};
+			d = rand() % count_lines;
+			e = rand() % count_lines;
+			f = rand() % count_lines;
+			g = rand() % count_lines;
+		} while (a == b || a == c || a == d || a == e || a == f || a == g || b == c || b == d || b == e || b == f ||
+			b == g || c == d || c == e || c == f || c == g || d == e || d == f || d == g || e == f || e == g ||
+			f == g || a == idx || b == idx || c == idx || d == idx || e == idx || f == idx || g == idx);
+		int position = rand() % 8;
+		int ids[] = { a,b,c,d,e,f,g };
+		char hints[] = {'a','s','d','f','j','k','l',';'};
 		int next = 0;
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 8; i++) {
 			if (i == position) {
 				printf("\t%c: %s\n",hints[i],list[idx].Chinese);
 			}
@@ -148,7 +154,7 @@ void mc() {
 			system("color C");
 		}
 		system("cls");
-		printf("\n\n\n\n");
+		printf("\n\n\n");
 		for (int i = 0; i < 26 - ((len_E + 1) >> 1); i++) {
 			printf(" ");
 		}
